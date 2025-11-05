54eeaf4528a320d8d1f23eb2ddbb3bc0006aac9e
jmonto98 <jmonto98@eafit.edu.co>
2025-11-04 19:12:40 -0500

    Refactor database initialization; move db instance to __init__.py files in auth_service and catalog_service

--- auth_service/models/user.py
+++ auth_service/models/user.py
@@ -1,7 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
+from models import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
--- catalog_service/models/book.py
+++ catalog_service/models/book.py
@@ -1,11 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-def init_db(app_db):
-    global db
-    db = app_db
-
+from models import db
 class Book(db.Model):
     __tablename__ = 'book'
     id = db.Column(db.Integer, primary_key=True)
